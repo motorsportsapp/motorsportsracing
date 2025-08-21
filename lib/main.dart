ca5eb623c50a389baf69eaa0e1444819842d09f5
robiulsunnyemon <robiulsunyemon@gmail.com>
2025-08-21 18:12:25 +0600

    update ui 3

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ Future<void> main() async {
   final String? uid = await SharedPrefHelper.getUid();
   runApp(
     MotorSportEasyApp(
-      initialRoute:Routes.RACE_ADMIN
+      initialRoute:Routes.LOGIN_DASHBOARD
     ),
   );
 }
